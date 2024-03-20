343b54cec064d18b4c433dfe20b9d2b63b1620e9
Caesar <sandorcezar@yahoo.com>
2024-03-20 15:06:25 +0200

    Currency exchanger

--- src/main/java/ro/scoalainformala/model/Currency.java
+++ src/main/java/ro/scoalainformala/model/Currency.java
@@ -75,38 +75,38 @@ public class Currency {
 
     // Static block to initialize the map with currency code and full name pairs
     static {
-        CURRENCY_FULL_NAMES.put("AED", " (United Arab Emirates Dirham)");
-        CURRENCY_FULL_NAMES.put("AUD", " (Australian Dollar)");
-        CURRENCY_FULL_NAMES.put("BGN", " (Bulgarian Lev)");
-        CURRENCY_FULL_NAMES.put("BRL", " (Brazilian Rel)");
-        CURRENCY_FULL_NAMES.put("CAD", " (Canadian Dollar)");
-        CURRENCY_FULL_NAMES.put("CHF", " (Swiss Franc)");
-        CURRENCY_FULL_NAMES.put("CNY", " (China Yuan/Renminbi)");
-        CURRENCY_FULL_NAMES.put("CZK", " (Czech Koruna)");
-        CURRENCY_FULL_NAMES.put("DKK", " (Danish Krone)");
-        CURRENCY_FULL_NAMES.put("EGP", " (Egyptian Pound)");
-        CURRENCY_FULL_NAMES.put("EUR", " (Euro)");
-        CURRENCY_FULL_NAMES.put("GBP", " (Great Britain Pound)");
-        CURRENCY_FULL_NAMES.put("HUF", " (Hungarian Forint)");
-        CURRENCY_FULL_NAMES.put("INR", " (Indian Rupee)");
-        CURRENCY_FULL_NAMES.put("JPY", " (Japanese Yen)");
-        CURRENCY_FULL_NAMES.put("KRW", " (Korean Won)");
-        CURRENCY_FULL_NAMES.put("MDL", " (Moldova Leu)");
-        CURRENCY_FULL_NAMES.put("MXN", " (Mexican Peso)");
-        CURRENCY_FULL_NAMES.put("NOK", " (Norwegian Kroner)");
-        CURRENCY_FULL_NAMES.put("NZD", " (New Zealand Dollar)");
-        CURRENCY_FULL_NAMES.put("PLN", " (Polish Zloty)");
-        CURRENCY_FULL_NAMES.put("RON", " (Leu Romanesc)");
-        CURRENCY_FULL_NAMES.put("RSD", " (Serbian Dinar)");
-        CURRENCY_FULL_NAMES.put("RUB", " (Russian Rouble)");
-        CURRENCY_FULL_NAMES.put("SEK", " (Swedish Krona)");
-        CURRENCY_FULL_NAMES.put("THB", " (Thai Baht)");
-        CURRENCY_FULL_NAMES.put("TRY", " (Turkish Lira)");
-        CURRENCY_FULL_NAMES.put("UAH", " (Ukraine Hryvnia)");
-        CURRENCY_FULL_NAMES.put("USD", " (USA Dollar)");
-        CURRENCY_FULL_NAMES.put("XAU", " (Gold (oz))");
-        CURRENCY_FULL_NAMES.put("XDR", " (Special Drawing Rights)");
-        CURRENCY_FULL_NAMES.put("ZAR", " (South African Rand)");
+        CURRENCY_FULL_NAMES.put("AED", " - United Arab Emirates Dirham");
+        CURRENCY_FULL_NAMES.put("AUD", " - Australian Dollar");
+        CURRENCY_FULL_NAMES.put("BGN", " - Bulgarian Lev");
+        CURRENCY_FULL_NAMES.put("BRL", " - Brazilian Rel");
+        CURRENCY_FULL_NAMES.put("CAD", " - Canadian Dollar");
+        CURRENCY_FULL_NAMES.put("CHF", " - Swiss Franc");
+        CURRENCY_FULL_NAMES.put("CNY", " - China Yuan/Renminbi");
+        CURRENCY_FULL_NAMES.put("CZK", " - Czech Koruna");
+        CURRENCY_FULL_NAMES.put("DKK", " - Danish Krone");
+        CURRENCY_FULL_NAMES.put("EGP", " - Egyptian Pound");
+        CURRENCY_FULL_NAMES.put("EUR", " - Euro");
+        CURRENCY_FULL_NAMES.put("GBP", " - Great Britain Pound");
+        CURRENCY_FULL_NAMES.put("HUF", " - Hungarian Forint");
+        CURRENCY_FULL_NAMES.put("INR", " - Indian Rupee");
+        CURRENCY_FULL_NAMES.put("JPY", " - Japanese Yen");
+        CURRENCY_FULL_NAMES.put("KRW", " - Korean Won");
+        CURRENCY_FULL_NAMES.put("MDL", " - Moldova Leu");
+        CURRENCY_FULL_NAMES.put("MXN", " - Mexican Peso");
+        CURRENCY_FULL_NAMES.put("NOK", " - Norwegian Kroner");
+        CURRENCY_FULL_NAMES.put("NZD", " - New Zealand Dollar");
+        CURRENCY_FULL_NAMES.put("PLN", " - Polish Zloty");
+        CURRENCY_FULL_NAMES.put("RON", " - Leu Romanesc");
+        CURRENCY_FULL_NAMES.put("RSD", " - Serbian Dinar");
+        CURRENCY_FULL_NAMES.put("RUB", " - Russian Rouble");
+        CURRENCY_FULL_NAMES.put("SEK", " - Swedish Krona");
+        CURRENCY_FULL_NAMES.put("THB", " - Thai Baht");
+        CURRENCY_FULL_NAMES.put("TRY", " - Turkish Lira");
+        CURRENCY_FULL_NAMES.put("UAH", " - Ukraine Hryvnia");
+        CURRENCY_FULL_NAMES.put("USD", " - USA Dollar");
+        CURRENCY_FULL_NAMES.put("XAU", " - Gold (oz)");
+        CURRENCY_FULL_NAMES.put("XDR", " - Special Drawing Rights");
+        CURRENCY_FULL_NAMES.put("ZAR", " - South African Rand");
     }
 
     // Method to retrieve the full name of a given currency code
